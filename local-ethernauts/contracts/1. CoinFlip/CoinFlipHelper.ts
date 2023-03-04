6ee4e37e1a60166e6da42bbfd4f2fa0800583e81
Zoumana Cisse <zoumanacisse692@gmail.com>
2023-03-04 15:39:14 -0800

    finished Force

--- local-ethernauts/contracts/1. CoinFlip/CoinFlipHelper.ts
+++ local-ethernauts/contracts/1. CoinFlip/CoinFlipHelper.ts
@@ -1,6 +1,3 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
-
 const helper = async (victim: any, attacker: any) => {
   // add code here that will help you pass the test
   let winCount = await victim.consecutiveWins();
